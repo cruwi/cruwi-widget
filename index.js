6a1d4e7cad6e614a90ae07556011c2596082d410
Andreas Rutjens <50319647+andreasdevjs@users.noreply.github.com>
2022-09-21 14:46:47 +0200

    version 1.0.8

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 (() => {
   
-  console.log('Script Init');
+  console.log('Cruwi Script Init');
 
   // Obtenemos el merchant name y la API KEY
   const currentScriptProcessed = document.currentScript;
   const merchantNameFromScript = new URLSearchParams(new URL(currentScriptProcessed.getAttribute('src')).search).get('merchantName');
   const merchantApiKeyFromScript = new URLSearchParams(new URL(currentScriptProcessed.getAttribute('src')).search).get('apiKey');
-
-  console.log('Merchant Name from Script: ', merchantNameFromScript);
-  console.log('API Key: ',merchantApiKeyFromScript);
+  const widgetTypeFromScript = new URLSearchParams(new URL(currentScriptProcessed.getAttribute('src')).search).get('widgetType');
 
   // Comprobamos nombre del merchant según la url
   const TLDs = ["ac", "ad", "ae", "aero", "af", "ag", "ai", "al", "am", "an", "ao", "aq", "ar", "arpa", "as", "asia", "at", "au", "aw", "ax", "az", "ba", "bb", "bd", "be", "bf", "bg", "bh", "bi", "biz", "bj", "bm", "bn", "bo", "br", "bs", "bt", "bv", "bw", "by", "bz", "ca", "cat", "cc", "cd", "cf", "cg", "ch", "ci", "ck", "cl", "cm", "cn", "co", "com", "coop", "cr", "cu", "cv", "cx", "cy", "cz", "de", "dj", "dk", "dm", "do", "dz", "ec", "edu", "ee", "eg", "er", "es", "et", "eu", "fi", "fj", "fk", "fm", "fo", "fr", "ga", "gb", "gd", "ge", "gf", "gg", "gh", "gi", "gl", "gm", "gn", "gov", "gp", "gq", "gr", "gs", "gt", "gu", "gw", "gy", "hk", "hm", "hn", "hr", "ht", "hu", "id", "ie", "il", "im", "in", "info", "int", "io", "iq", "ir", "is", "it", "je", "jm", "jo", "jobs", "jp", "ke", "kg", "kh", "ki", "km", "kn", "kp", "kr", "kw", "ky", "kz", "la", "lb", "lc", "li", "lk", "lr", "ls", "lt", "lu", "lv", "ly", "ma", "mc", "md", "me", "mg", "mh", "mil", "mk", "ml", "mm", "mn", "mo", "mobi", "mp", "mq", "mr", "ms", "mt", "mu", "museum", "mv", "mw", "mx", "my", "mz", "na", "name", "nc", "ne", "net", "nf", "ng", "ni", "nl", "no", "np", "nr", "nu", "nz", "om", "org", "pa", "pe", "pf", "pg", "ph", "pk", "pl", "pm", "pn", "pr", "pro", "ps", "pt", "pw", "py", "qa", "re", "ro", "rs", "ru", "rw", "sa", "sb", "sc", "sd", "se", "sg", "sh", "si", "sj", "sk", "sl", "sm", "sn", "so", "sr", "st", "su", "sv", "sy", "sz", "tc", "td", "tel", "tf", "tg", "th", "tj", "tk", "tl", "tm", "tn", "to", "tp", "tr", "travel", "tt", "tv", "tw", "tz", "ua", "ug", "uk", "us", "uy", "uz", "va", "vc", "ve", "vg", "vi", "vn", "vu", "wf", "ws", "xxx", "ye", "yt", "za", "zm", "zw"].join();
@@ -27,9 +25,24 @@
     }
   }
 
-  let merchantNameFromUrl = getDomain(location.host)
-  console.log('Merchant Name from URL: ',merchantNameFromUrl);
+  let merchantNameFromUrl = getDomain(location.host);
 
-  console.log('1.0.7');
+  // Comprobamos si hay widget y qué tipo de widget se solicita
+  const widgetElement = document.querySelector('[data-cruwi-widget-type]');
+
+  if(widgetElement) {
+    const widgetType = widgetElement.dataset.cruwiWidgetType;
+    if(widgetType === 'pdp') {
+      alert('es pdp');
+    } else if(widgetType === 'section') {
+      alert('es section');
+    } else if(widgetType === 'checkout') {
+      alert('es checkout');
+    } else {
+      console.error('This type does not exists');
+    }
+  } else {
+    console.error('There is no widget to show');
+  }
 
 })();
