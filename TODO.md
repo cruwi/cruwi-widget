4a4a35ce05aff3036f25ada1c911edc038bad49b
Andreas Rutjens <50319647+andreasdevjs@users.noreply.github.com>
2022-09-22 19:48:16 +0200

    version 1.0.12

--- TODO.md
+++ TODO.md
@@ -2,11 +2,16 @@
 
 - Ver si es Prestahop, Shopify o WooCommerce
 - Comprobar el acceso a esas variables globales de esos ecommerce y copiar los valores requeridos
-- Comprobar si es PDP, Sección o Checkout (con data-cruwi-type="pdp/checkout/section")
 - Mostrar banners correspondientes
 - Abrir un modal explicativo full-screen on-click en dichos banners
 - Coger dinámicamente el logo, cashback y configuraciones de cada marca en dicho modal y banner.
 - Coger dinámicamente valores por url o data x-data
+- Dar la opción de SÓLO Tiktok o SÓLO Enlace
 
 - Generar el enlace a compartir del cliente con sus datos asociados
 - Si es un producto tiktokeable, ofrecer la opción
+
+
+## Task Done
+- Comprobar si es PDP, Sección o Checkout (con data-cruwi-type="pdp/checkout/section") ✅ 
+- 
--- index.js
+++ index.js
@@ -4,10 +4,10 @@
 
   // Obtenemos el merchant name y la API KEY
   const currentScriptProcessed = document.currentScript;
-  console.log(currentScriptProcessed)
-  // const merchantNameFromScript = new URLSearchParams(new URL(currentScriptProcessed.getAttribute('src')).search).get('merchantName');
-  // const merchantApiKeyFromScript = new URLSearchParams(new URL(currentScriptProcessed.getAttribute('src')).search).get('apiKey');
-  // const widgetTypeFromScript = new URLSearchParams(new URL(currentScriptProcessed.getAttribute('src')).search).get('widgetType');
+  console.log(currentScriptProcessed);
+  const merchantNameFromScript = new URLSearchParams(new URL(currentScriptProcessed.getAttribute('src')).search).get('merchantName');
+  const merchantApiKeyFromScript = new URLSearchParams(new URL(currentScriptProcessed.getAttribute('src')).search).get('apiKey');
+  const widgetTypeFromScript = new URLSearchParams(new URL(currentScriptProcessed.getAttribute('src')).search).get('widgetType');
 
   // Comprobamos nombre del merchant según la url
   const TLDs = ["ac", "ad", "ae", "aero", "af", "ag", "ai", "al", "am", "an", "ao", "aq", "ar", "arpa", "as", "asia", "at", "au", "aw", "ax", "az", "ba", "bb", "bd", "be", "bf", "bg", "bh", "bi", "biz", "bj", "bm", "bn", "bo", "br", "bs", "bt", "bv", "bw", "by", "bz", "ca", "cat", "cc", "cd", "cf", "cg", "ch", "ci", "ck", "cl", "cm", "cn", "co", "com", "coop", "cr", "cu", "cv", "cx", "cy", "cz", "de", "dj", "dk", "dm", "do", "dz", "ec", "edu", "ee", "eg", "er", "es", "et", "eu", "fi", "fj", "fk", "fm", "fo", "fr", "ga", "gb", "gd", "ge", "gf", "gg", "gh", "gi", "gl", "gm", "gn", "gov", "gp", "gq", "gr", "gs", "gt", "gu", "gw", "gy", "hk", "hm", "hn", "hr", "ht", "hu", "id", "ie", "il", "im", "in", "info", "int", "io", "iq", "ir", "is", "it", "je", "jm", "jo", "jobs", "jp", "ke", "kg", "kh", "ki", "km", "kn", "kp", "kr", "kw", "ky", "kz", "la", "lb", "lc", "li", "lk", "lr", "ls", "lt", "lu", "lv", "ly", "ma", "mc", "md", "me", "mg", "mh", "mil", "mk", "ml", "mm", "mn", "mo", "mobi", "mp", "mq", "mr", "ms", "mt", "mu", "museum", "mv", "mw", "mx", "my", "mz", "na", "name", "nc", "ne", "net", "nf", "ng", "ni", "nl", "no", "np", "nr", "nu", "nz", "om", "org", "pa", "pe", "pf", "pg", "ph", "pk", "pl", "pm", "pn", "pr", "pro", "ps", "pt", "pw", "py", "qa", "re", "ro", "rs", "ru", "rw", "sa", "sb", "sc", "sd", "se", "sg", "sh", "si", "sj", "sk", "sl", "sm", "sn", "so", "sr", "st", "su", "sv", "sy", "sz", "tc", "td", "tel", "tf", "tg", "th", "tj", "tk", "tl", "tm", "tn", "to", "tp", "tr", "travel", "tt", "tv", "tw", "tz", "ua", "ug", "uk", "us", "uy", "uz", "va", "vc", "ve", "vg", "vi", "vn", "vu", "wf", "ws", "xxx", "ye", "yt", "za", "zm", "zw"].join();
@@ -18,7 +18,6 @@
       parts.shift()
     }
     var ln = parts.length, i = ln, minLength = parts[parts.length-1].length, part
-
     while(part = parts[--i]){
       if (i === 0 || i < ln-2 || part.length < minLength || TLDs.indexOf(part) < 0){
         return part
@@ -158,7 +157,7 @@
 
     const cruwiModalFooterWrapperImageLogo = document.createElement('img');
     cruwiModalFooterWrapperImageLogo.setAttribute('src', 'https://uploads-ssl.webflow.com/62ea5c239bacb85550bf44ea/6328573bad60f760ac2b5fbb_CRUWI%20(3).svg');
-    cruwiModalFooterWrapperImageLogo.setAttribute('width', '24');
+    cruwiModalFooterWrapperImageLogo.setAttribute('width', '28');
     cruwiModalFooterWrapperImageLogo.setAttribute('alt', 'Powered by cruwi.com');
     cruwiModalFooterWrapper.appendChild(cruwiModalFooterWrapperImageLogo);
     
@@ -218,6 +217,7 @@
     const cruwiStyleTag = document.createElement('style');
     document.head.append(cruwiStyleTag);
     cruwiStyleTag.innerHTML = `
+
       .cruwi-pdp-widget {
         background-color: white;
         color: #111;
@@ -242,8 +242,10 @@
         box-shadow: 1px 1px 20px #0000001c;
         font-family: 'Poppins', sans-serif;
         z-index: 999999;
-        max-height: 90vh;
+        min-height: 90vh;
         overflow: auto;
+        display: flex;
+        flex-direction: column;
       }
       
       .cruwi-modal.cruwi-modal-active {
@@ -274,7 +276,7 @@
       }
 
       .cruwi-modal-logo {
-        width: 50px;
+        width: 16px;
       }
       
       .cruwi-modal-close-button {
@@ -291,6 +293,7 @@
       .cruwi-modal-footer {
         border-top: 1px solid #ebe5e5;
         padding: 3px;
+        margin-top: auto;
       }
 
       .cruwi-modal-footer-wrapper {
@@ -298,6 +301,7 @@
         flex-direction: row;
         justify-content: center;
         align-items: center;
+        font-family: 'Proxima Nova', sans-serif;
       }
 
       .cruwi-modal-footer-wrapper-text {
